9ae2609057603fd051b193f756c2255f7307bc27
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2024-11-06 14:24:06 +0100

    P4ADEV-1378-initialize-app-structure

---
+++ build.gradle.kts
@@ -0,0 +1,86 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.5"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "it.gov.pagopa.payhub"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+
+repositories {
+	mavenCentral()
+}
+
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+
+
+val janinoVersion = "3.1.12"
+val wiremockVersion = "3.5.4"
+val snakeYamlVersion = "2.0"
+val hibernateValidatorVersion = "8.0.1.Final"
+
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.codehaus.janino:janino:$janinoVersion")
+	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+	implementation("org.hibernate.validator:hibernate-validator:$hibernateValidatorVersion")
+
+	// Security fixes
+	implementation("org.yaml:snakeyaml:$snakeYamlVersion")
+
+	//	Testing
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.junit.jupiter:junit-jupiter-api")
+	testImplementation("org.junit.jupiter:junit-jupiter-engine")
+	testImplementation("org.mockito:mockito-core")
+	testImplementation ("org.projectlombok:lombok")
+	testImplementation ("org.wiremock:wiremock-standalone:$wiremockVersion")
+
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+}
+
+
+val projectInfo = mapOf(
+		"artifactId" to project.name,
+		"version" to project.version
+)
+
+tasks {
+	val processResources by getting(ProcessResources::class) {
+		filesMatching("**/application.yml") {
+			expand(projectInfo)
+		}
+	}
+}
+
+configurations {
+	compileClasspath {
+		resolutionStrategy.activateDependencyLocking()
+	}
+}
+
+configure<SourceSetContainer> {
+	named("main") {
+		java.srcDir("$projectDir/build/generated/src/main/java")
+	}
+}
