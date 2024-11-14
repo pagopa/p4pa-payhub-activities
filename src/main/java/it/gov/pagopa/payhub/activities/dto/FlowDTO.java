97866e318d6beadb6ea666ffb45578010a246b0d
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2024-11-14 11:59:36 +0100

    [P4ADEV-1347] fix flowDTO

--- src/main/java/it/gov/pagopa/payhub/activities/dto/FlowDTO.java
+++ src/main/java/it/gov/pagopa/payhub/activities/dto/FlowDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 @Builder
@@ -14,4 +15,20 @@ import java.io.Serializable;
 public class FlowDTO implements Serializable {
 
     private Long flowId;
+    private int version;
+    private OrganizationDTO orgId;
+    private RegistryStatusDTO statusId;
+    private String iuf;
+    private Long numTotalRows;
+    private Long numCorrectlyImportedRows;
+    private Date creationDate;
+    private Date lastChangeDate;
+    private boolean flagActive;
+    private String operatorName;
+    private Boolean flagSpontaneous;
+    private String filePathName;
+    private String fileName;
+    private Long pdfGenerated;
+    private String codRequestToken;
+    private String codError;
 }
