c6d519474d611c9fa914606749368f77ba127dd3
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2024-11-14 10:52:34 +0100

    add-code-review

--- src/main/java/it/gov/pagopa/payhub/activities/activity/VerifyAuthorizationInstallmentsActivity.java
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/VerifyAuthorizationInstallmentsActivity.java
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class VerifyAuthorizationInstallments {
+public class VerifyAuthorizationInstallmentsActivity {
 
     private final OrganizationInstallmentTypeDao organizationInstallmentTypeDao;
 
-    public VerifyAuthorizationInstallments(OrganizationInstallmentTypeDao organizationInstallmentTypeDao) {
+    public VerifyAuthorizationInstallmentsActivity(OrganizationInstallmentTypeDao organizationInstallmentTypeDao) {
         this.organizationInstallmentTypeDao = organizationInstallmentTypeDao;
     }
 
