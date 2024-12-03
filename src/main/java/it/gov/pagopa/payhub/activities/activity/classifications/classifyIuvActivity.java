071f8b15ce89220f995d4925ed1527feccd4a67b
marco.matteucci@eng.it <marco.matteucci@eng.it>
2024-12-03 10:20:48 +0100

    feat:P4ADEV-1584-IUV-classification-activity-interface

---
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/classifications/classifyIuvActivity.java
@@ -0,0 +1,17 @@
+package it.gov.pagopa.payhub.activities.activity.classifications;
+/**
+ * Interface for defining an activity to process payment classifications.
+ */
+public interface classifyIuvActivity {
+
+    /**
+     * Processes IUV classification based on the provided parameters.
+     *
+     * @param organizationId the unique identifier of the organization
+     * @param IUV            the unique identifier of the payment (IUV)
+     * @param receiptId      the identifier of the receipt associated with the payment
+     * @param transferIndex  the index of the transfer to be classified
+     * @return true if the classification is successful, false otherwise
+     */
+    boolean classify(String organizationId, String IUV, String receiptId, int transferIndex);
+}
