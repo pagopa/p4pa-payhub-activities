bcd05b684365e88e9d8a3e6087866a708b358302
marcomatteuccieng <marco.matteucci@eng.it>
2024-12-03 10:55:21 +0100

    Update src/main/java/it/gov/pagopa/payhub/activities/activity/classifications/classifyIuvActivity.java
    Co-authored-by: antonioT90 <34568575+antonioT90@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/payhub/activities/activity/classifications/classifyIuvActivity.java
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/classifications/classifyIuvActivity.java
@@ -8,7 +8,7 @@ public interface IuvClassificationActivity {
      * Processes IUV classification based on the provided parameters.
      *
      * @param organizationId the unique identifier of the organization
-     * @param IUV            the unique identifier of the payment (IUV)
+     * @param iuv            the unique identifier of the payment (IUV)
      * @param receiptId      the identifier of the receipt associated with the payment
      * @param transferIndex  the index of the transfer to be classified
      * @return true if the classification is successful, false otherwise
