10a9dc316310cdb2910395f34bca1089f1fa4934
Silvano Tamburini <silvano.tamburini@eng.it>
2024-12-07 12:36:56 +0100

    recover wrong deletion of exception class

---
+++ src/main/java/it/gov/pagopa/payhub/activities/exception/IngestionFlowNotFoundException.java
@@ -0,0 +1,19 @@
+package it.gov.pagopa.payhub.activities.exception;
+
+/**
+ * A custom exception that indicates the absence of an expected ingestion flow
+ * in the application's processing logic.
+ *
+ */
+public class IngestionFlowNotFoundException extends ActivitiesException {
+
+	/**
+	 * Constructs a new {@code IngestionFlowFileNotFoundException} with the specified detail message.
+	 *
+	 * @param message the detail message explaining the cause of the exception.
+	 */
+	public IngestionFlowNotFoundException(String message) {
+		super(message);
+	}
+}
+
