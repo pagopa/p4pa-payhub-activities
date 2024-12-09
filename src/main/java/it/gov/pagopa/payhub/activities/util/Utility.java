ab3bf87ad3e3208bcedafa64eb6ba651258e74b6
Silvano Tamburini <silvano.tamburini@eng.it>
2024-12-09 23:53:07 +0100

    utilities

---
+++ src/main/java/it/gov/pagopa/payhub/activities/util/Utility.java
@@ -0,0 +1,10 @@
+package it.gov.pagopa.payhub.activities.util;
+
+public class Utility {
+    public static boolean isNullOrEmptyString(String str){
+        return(str == null || str.isEmpty());
+    }
+    public static boolean isNotNullOrEmpty(String str){
+        return ! isNullOrEmptyString(str);
+    }
+}
