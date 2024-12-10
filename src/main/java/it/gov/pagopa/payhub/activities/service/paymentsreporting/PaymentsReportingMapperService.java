cae63bdb5557a75e5281e243cb5ea26ea1a0a3a5
macacia <marco.cacia@eng.it>
2024-12-10 14:05:45 +0100

    P4ADEV-1542 removed unused import

--- src/main/java/it/gov/pagopa/payhub/activities/service/paymentsreporting/PaymentsReportingMapperService.java
+++ src/main/java/it/gov/pagopa/payhub/activities/service/paymentsreporting/PaymentsReportingMapperService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Service class responsible for mapping flow data (`CtFlussoRiversamento`) and ingestion metadata
