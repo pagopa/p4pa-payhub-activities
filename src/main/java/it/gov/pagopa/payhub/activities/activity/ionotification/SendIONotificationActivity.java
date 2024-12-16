0f98edce0a0bc2c1837eee6fa4f4d25dd6670fb4
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2024-12-16 11:27:56 +0100

    P4ADEV-1506-activity-io-notification

--- src/main/java/it/gov/pagopa/payhub/activities/activity/ionotification/SendIONotificationActivity.java
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/ionotification/SendIONotificationActivity.java
@@ -2,7 +2,17 @@ package it.gov.pagopa.payhub.activities.activity.ionotification;
 
 import it.gov.pagopa.pu.p4paionotification.model.generated.NotificationQueueDTO;
 
+/**
+ * This interface provides a method for sending notification messages to the microservice IO Notification.
+ */
 public interface SendIONotificationActivity {
 
+    /**
+     * Sends a notification message to the IO Notification API.
+     *
+     * @param notificationQueueDTO the notification data to be sent.
+     */
     void sendMessage(NotificationQueueDTO notificationQueueDTO);
 }
+
+
