9cbdbc1eeb461bc43bb80d77754e2eb76f5f4b68
Silvano Tamburini <silvano.tamburini@eng.it>
2024-12-17 17:50:01 +0100

    delete unused imports

--- src/test/java/it/gov/pagopa/payhub/activities/activity/classifications/ClearClassifyIufActivityTest.java
+++ src/test/java/it/gov/pagopa/payhub/activities/activity/classifications/ClearClassifyIufActivityTest.java
@@ -2,13 +2,10 @@ package it.gov.pagopa.payhub.activities.activity.classifications;
 
 import it.gov.pagopa.payhub.activities.dao.ClassifyDao;
 import it.gov.pagopa.payhub.activities.utility.Utilities;
-import net.bytebuddy.implementation.bytecode.Throw;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
