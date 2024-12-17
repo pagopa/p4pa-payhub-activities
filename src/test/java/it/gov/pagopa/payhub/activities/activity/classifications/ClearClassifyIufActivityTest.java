fa2e61e467860fec0863ab381c63ddac166ec2ef
Silvano Tamburini <silvano.tamburini@eng.it>
2024-12-17 17:13:43 +0100

    tests refactoring

--- src/test/java/it/gov/pagopa/payhub/activities/activity/classifications/ClearClassifyIufActivityTest.java
+++ src/test/java/it/gov/pagopa/payhub/activities/activity/classifications/ClearClassifyIufActivityTest.java
@@ -1,16 +1,24 @@
 package it.gov.pagopa.payhub.activities.activity.classifications;
 
 import it.gov.pagopa.payhub.activities.dao.ClassifyDao;
+import it.gov.pagopa.payhub.activities.utility.Utilities;
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ClearClassifyIufActivityTest {
+    private static final Long organizationId = 1L;
+    private static final String iuf = "IUF";
+
     @Mock
     private ClassifyDao classifyDao;
 
@@ -22,10 +30,14 @@ class ClearClassifyIufActivityTest {
     }
 
     @Test
-    void deleteClassification() {
-        Long organizationId = 1L;
-        String iuf = "IUF";
+    void deleteClassificationSuccess() {
         assertDoesNotThrow(() -> clearClassifyIufActivity.deleteClassificationByIuf(organizationId,iuf));
+    }
+
+    @Test
+    void deleteClassificationFailed() {
+        when(classifyDao.deleteClassificationByIuf(organizationId, iuf, Utilities.CLASSIFICATION.TES_NO_MATCH.getValue())).thenReturn(false);
+        assertFalse(clearClassifyIufActivity.deleteClassificationByIuf(organizationId,iuf));
     }
 
 }
