b8655d28ada40d1a51f4f4507c5ef3fa656bafc3
Silvano Tamburini <silvano.tamburini@eng.it>
2024-12-18 14:55:09 +0100

    added temporal annotations in IufClassificationActivity interface

--- src/main/java/it/gov/pagopa/payhub/activities/activity/classifications/IufClassificationActivity.java
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/classifications/IufClassificationActivity.java
@@ -1,10 +1,13 @@
 package it.gov.pagopa.payhub.activities.activity.classifications;
 
+import io.temporal.activity.ActivityInterface;
+import io.temporal.activity.ActivityMethod;
 import it.gov.pagopa.payhub.activities.dto.classifications.IufClassificationActivityResult;
 
 /**
  * Interface for defining an activity to process payment reporting classifications based on IUF.
  */
+@ActivityInterface
 public interface IufClassificationActivity {
 
     /**
@@ -14,5 +17,6 @@ public interface IufClassificationActivity {
      * @param iuf            the unique identifier of the payment reporting flow (IUF)
      * @return IufClassificationActivityResult containing PaymentsReportingDTO list and success flag
      */
+    @ActivityMethod
     IufClassificationActivityResult classify(Long organizationId, String iuf);
 }
