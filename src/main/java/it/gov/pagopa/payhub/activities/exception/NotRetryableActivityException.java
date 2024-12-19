eabb8c6d58edcf2b29425308093dc1656aadfc3c
Silvano Tamburini <silvano.tamburini@eng.it>
2024-12-19 09:02:39 +0100

    delete not used excepptions for coverage problams

--- src/main/java/it/gov/pagopa/payhub/activities/exception/NotRetryableActivityException.java
+++
@@ -1,6 +0,0 @@
-package it.gov.pagopa.payhub.activities.exception;
-
-public class NotRetryableActivityException extends RuntimeException {
-
-    public NotRetryableActivityException(String message) { super(message); }
-}
