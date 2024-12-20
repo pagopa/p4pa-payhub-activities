517d2d6c7aae8fa1e92c7917411404c1ddcc95a4
macacia <marco.cacia@eng.it>
2024-12-20 17:34:06 +0100

    P4ADEV-1635 resolved change request

--- src/main/java/it/gov/pagopa/payhub/activities/activity/classifications/TransferClassificationActivity.java
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/classifications/TransferClassificationActivity.java
@@ -4,20 +4,19 @@ import io.temporal.activity.ActivityInterface;
 import io.temporal.activity.ActivityMethod;
 
 /**
- * Interface for defining an activity to process payment classifications based on IUV.
+ * Interface for defining an activity to process Transfer classifications.
  */
 @ActivityInterface
 public interface TransferClassificationActivity {
 
     /**
-     * Processes IUV classification based on the provided parameters.
+     * Processes Transfer classification based on the provided parameters.
      *
      * @param orgId the unique identifier of the organization
      * @param iuv   the unique identifier of the payment (IUV)
      * @param iur   the identifier of the receipt associated with the payment
      * @param transferIndex  the index of the transfer to be classified
-     * @return true if the classification is successful, false otherwise
      */
     @ActivityMethod
-    boolean classify(Long orgId, String iuv, String iur, int transferIndex);
+    void classify(Long orgId, String iuv, String iur, int transferIndex);
 }
