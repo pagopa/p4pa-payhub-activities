f9462fa48a49364da5b39e6c385548f52f0c5efd
silvanotamburini <silvano.tamburini@eng.it>
2024-12-23 15:58:18 +0100

    feat: P4ADEV-1625 classifyiufactivity-retrieve-iuvs-save (#48)

--- src/main/java/it/gov/pagopa/payhub/activities/dao/ClassificationDao.java
+++ src/main/java/it/gov/pagopa/payhub/activities/dao/ClassificationDao.java
@@ -1,18 +1,27 @@
 package it.gov.pagopa.payhub.activities.dao;
 
+import it.gov.pagopa.payhub.activities.dto.classifications.ClassificationDTO;
 import it.gov.pagopa.payhub.activities.enums.ClassificationsEnum;
 
 /**
- * Data Access Object interface for saving payments classification
+ * Data Access Object interface  for saving payments classification
  */
 public interface ClassificationDao {
 	/**
+	 * save classification
 	 *
-	 * @param organizationId organization id
-	 * @param iuf fow identifier
-	 * @param classification classification to delete
-	 * @return true for success deletion
-	 */
+     * @param classificationDTO dto classification  to save
+     */
+	void save(ClassificationDTO classificationDTO);
+
+    /**
+     * delete classification
+     *
+     * @param organizationId organization id
+     * @param iuf fow identifier
+     * @param classification classification to delete
+     * @return true for success deletion
+     */
 	boolean deleteClassificationByIuf(Long organizationId, String iuf, ClassificationsEnum classification);
 
 	/**
---
+++ src/main/java/it/gov/pagopa/payhub/activities/dto/classifications/IufClassificationActivityResult.java
@@ -0,0 +1,18 @@
+package it.gov.pagopa.payhub.activities.dto.classifications;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class IufClassificationActivityResult {
+    private Long organizationId;
+    private List<Transfer2ClassifyDTO> transfers2classify;
+    private boolean success;
+}
