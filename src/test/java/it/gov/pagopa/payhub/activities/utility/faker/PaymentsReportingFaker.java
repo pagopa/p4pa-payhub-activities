b8b7327352ad604b3b41c28e5317983eabc9da44
Silvano Tamburini <silvano.tamburini@eng.it>
2025-01-02 14:43:07 +0100

    add field missing in faker

--- src/test/java/it/gov/pagopa/payhub/activities/utility/faker/PaymentsReportingFaker.java
+++ src/test/java/it/gov/pagopa/payhub/activities/utility/faker/PaymentsReportingFaker.java
@@ -7,6 +7,7 @@ public class PaymentsReportingFaker {
     public static PaymentsReportingDTO buildClassifyResultDTO(){
         return PaymentsReportingDTO.builder()
                 .organizationId(1L)
+                .flowIdentifierCode("IUF")
                 .creditorReferenceId("IUV")
                 .regulationUniqueIdentifier("IUR")
                 .transferIndex(1)
