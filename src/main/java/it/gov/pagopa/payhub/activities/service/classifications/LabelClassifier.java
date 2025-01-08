29f26b20745675afd2b56a28b8aa11963917167d
macacia <marco.cacia@eng.it>
2025-01-08 12:36:12 +0100

    P4ADEV-1636 added java docs

--- src/main/java/it/gov/pagopa/payhub/activities/service/classifications/LabelClassifier.java
+++ src/main/java/it/gov/pagopa/payhub/activities/service/classifications/LabelClassifier.java
@@ -5,7 +5,23 @@ import it.gov.pagopa.payhub.activities.dto.paymentsreporting.PaymentsReportingDT
 import it.gov.pagopa.payhub.activities.dto.treasury.TreasuryDTO;
 import it.gov.pagopa.payhub.activities.enums.ClassificationsEnum;
 
+/**
+ * Interface for defining classification logic for transfers based on provided data.
+ *
+ * <p>Implementations of this interface contain specific rules to evaluate
+ * and determine a classification label ({@link ClassificationsEnum}). Each implementation
+ * should handle a particular condition or set of conditions to decide the appropriate label.
+ */
 public interface LabelClassifier {
 
+	/**
+	 * Defines a classification label ({@link ClassificationsEnum}) based on the given data.
+	 *
+	 * @param transferDTO the transfer data.
+	 * @param paymentsReportingDTO the payment reporting data.
+	 * @param treasuryDTO the treasury data.
+	 * @return the classification label if conditions are met, or {@code null} if no label is applicable.
+	 */
 	ClassificationsEnum define(TransferDTO transferDTO, PaymentsReportingDTO paymentsReportingDTO, TreasuryDTO treasuryDTO);
 }
+
