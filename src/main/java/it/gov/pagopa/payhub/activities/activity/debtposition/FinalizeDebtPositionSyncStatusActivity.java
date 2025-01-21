4b969371892b2d7a8e39744f718fef92227bd0c4
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-01-21 11:24:48 +0100

    [P4ADEV-1902] activity interface of finalizeDebtPositionSyncStatus

--- src/main/java/it/gov/pagopa/payhub/activities/activity/debtposition/FinalizeDebtPositionSyncStatusActivity.java
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/debtposition/FinalizeDebtPositionSyncStatusActivity.java
@@ -1,7 +1,7 @@
 package it.gov.pagopa.payhub.activities.activity.debtposition;
 
 import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionDTO;
-import it.gov.pagopa.pu.debtposition.dto.generated.IudSyncStatusUpdateDTO;
+import it.gov.pagopa.pu.debtposition.dto.generated.IupdSyncStatusUpdateDTO;
 
 import java.util.Map;
 
@@ -14,8 +14,8 @@ public interface FinalizeDebtPositionSyncStatusActivity {
      * Finalizes the update of the debt position status from the installments
      *
      * @param debtPositionId the identifier of the debt position to be updated
-     * @param syncStatusDTO the map of IUD and {@link IudSyncStatusUpdateDTO} about new status and IUPD PagoPa of installment
+     * @param syncStatusDTO the map of IUD and {@link IupdSyncStatusUpdateDTO} about new status and IUPD PagoPa of installment
      */
-    DebtPositionDTO finalizeDebtPositionSyncStatus(Long debtPositionId, Map<String, IudSyncStatusUpdateDTO> syncStatusDTO);
+    DebtPositionDTO finalizeDebtPositionSyncStatus(Long debtPositionId, Map<String, IupdSyncStatusUpdateDTO> syncStatusDTO);
 
 }
