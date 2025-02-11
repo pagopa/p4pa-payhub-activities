a4b43d56aeda60659a8857736b5d07c58ef8bc21
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-02-11 17:27:46 +0100

    fix: Externalize debt position duedate extraction (#118)

---
+++ src/main/java/it/gov/pagopa/payhub/activities/util/DebtPositionUtilities.java
@@ -0,0 +1,23 @@
+package it.gov.pagopa.payhub.activities.util;
+
+import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionDTO;
+import it.gov.pagopa.pu.debtposition.dto.generated.InstallmentDTO;
+
+import java.time.OffsetDateTime;
+import java.util.Comparator;
+import java.util.Objects;
+
+public class DebtPositionUtilities {
+    private DebtPositionUtilities(){}
+
+    /** It will return the min dueDate of all active installments */
+    public static OffsetDateTime calcDebtPositionNextDueDate(DebtPositionDTO debtPositionDTO){
+        return debtPositionDTO.getPaymentOptions().stream()
+                .flatMap(po ->po.getInstallments().stream())
+                .filter(i -> InstallmentDTO.StatusEnum.UNPAID.equals(i.getStatus()))
+                .map(InstallmentDTO::getDueDate)
+                .filter(Objects::nonNull)
+                .min(Comparator.naturalOrder())
+                .orElse(null);
+    }
+}
