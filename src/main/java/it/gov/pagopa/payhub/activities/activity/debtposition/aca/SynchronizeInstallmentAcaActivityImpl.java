5006380c85067f09dd79591d67953d52e1bc57e7
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-02-12 09:40:00 +0100

    feat: DebtPosition sync activities (#119)

--- src/main/java/it/gov/pagopa/payhub/activities/activity/debtposition/aca/SynchronizeInstallmentAcaActivityImpl.java
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/debtposition/aca/SynchronizeInstallmentAcaActivityImpl.java
@@ -2,9 +2,11 @@ package it.gov.pagopa.payhub.activities.activity.debtposition.aca;
 
 import it.gov.pagopa.payhub.activities.connector.pagopapayments.AcaService;
 import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Lazy
 @Service
 public class SynchronizeInstallmentAcaActivityImpl implements SynchronizeInstallmentAcaActivity {
@@ -17,6 +19,7 @@ public class SynchronizeInstallmentAcaActivityImpl implements SynchronizeInstall
 
     @Override
     public void synchronizeInstallmentAca(DebtPositionDTO debtPosition, String iud) {
+        log.info("Synchronizing IUD {} of DebtPosition {} with ACA", iud, debtPosition.getDebtPositionId());
         acaService.syncInstallmentAca(iud, debtPosition);
     }
 }
---
+++ src/main/java/it/gov/pagopa/payhub/activities/connector/pagopapayments/GpdService.java
@@ -0,0 +1,18 @@
+package it.gov.pagopa.payhub.activities.connector.pagopapayments;
+
+import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionDTO;
+
+/**
+ * This interface provides a method for synchronize installment of a debt position on GPD service
+ */
+public interface GpdService {
+
+    /**
+     * Synchronize an installment of debt position on GPD
+     *
+     * @param iud the IUD of installment to be synchronized on GPD
+     * @param debtPositionDTO the debt position related to the installment.
+     */
+    void syncInstallmentGpd(String iud, DebtPositionDTO debtPositionDTO);
+
+}
