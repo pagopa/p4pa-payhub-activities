bf28b1dcb0b1a7c9b6b30ff1c73e3943f2fac653
macacia <marco.cacia@eng.it>
2025-02-28 18:31:14 +0100

    feat: P4ADEV-2101 handle payment outcome code 9 to generate dummy receipt (#144)

---
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/receipt/PaymentsReportingImplicitReceiptHandlerActivity.java
@@ -0,0 +1,20 @@
+package it.gov.pagopa.payhub.activities.activity.ingestionflow.receipt;
+
+import io.temporal.activity.ActivityInterface;
+import io.temporal.activity.ActivityMethod;
+import it.gov.pagopa.payhub.activities.dto.classifications.PaymentsReportingTransferDTO;
+
+/**
+ * Interface for the PaymentsReportingImplicitReceiptHandlerActivity.
+ * Defines methods for generate dummy Receipt due to outcome code (8 or 9) in Payments Reporting.
+ */
+@ActivityInterface
+public interface PaymentsReportingImplicitReceiptHandlerActivity {
+	/**
+	 * Generate dummy receipt based on Payment Reporting data
+	 *
+	 * @param paymentsReportingTransferDTO Object contains Transfer Semantic Key and payment outcome code
+	 */
+	@ActivityMethod
+	void handle(PaymentsReportingTransferDTO paymentsReportingTransferDTO);
+}
