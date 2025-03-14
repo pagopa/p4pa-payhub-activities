515cfeee04f3610b6b7ba8fcd0869f8cab709c01
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2025-03-14 14:04:13 +0100

    P4ADEV-2089-fix-activity-io-notification

--- src/main/java/it/gov/pagopa/payhub/activities/exception/ionotification/IONotificationInvocationException.java
+++
@@ -1,8 +0,0 @@
-package it.gov.pagopa.payhub.activities.exception.ionotification;
-
-public class IONotificationInvocationException extends RuntimeException{
-
-    public IONotificationInvocationException(String message) {
-        super(message);
-    }
-}
