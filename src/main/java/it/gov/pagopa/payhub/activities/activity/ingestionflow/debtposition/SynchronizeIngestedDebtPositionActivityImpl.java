6e4eecb992ab23c2b0c39b81994f59986bda2242
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-03-14 15:10:26 +0100

    [P4ADEV-2343] impl SynchronizeIngestedDebtPositionActivity

--- src/main/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/debtposition/SynchronizeIngestedDebtPositionActivityImpl.java
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/debtposition/SynchronizeIngestedDebtPositionActivityImpl.java
@@ -1,8 +1,10 @@
 package it.gov.pagopa.payhub.activities.activity.ingestionflow.debtposition;
 
-import it.gov.pagopa.payhub.activities.service.debtposition.DebtPositionSearchService;
-import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionDTO;
+import it.gov.pagopa.payhub.activities.connector.debtposition.DebtPositionService;
+import it.gov.pagopa.payhub.activities.exception.InvalidValueException;
+import it.gov.pagopa.pu.debtposition.dto.generated.PagedDebtPositions;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
@@ -13,27 +15,49 @@ import java.util.List;
 @Service
 public class SynchronizeIngestedDebtPositionActivityImpl implements SynchronizeIngestedDebtPositionActivity {
 
-    private final DebtPositionSearchService debtPositionSearchService;
+    private final DebtPositionService debtPositionService;
+    private final Integer pageSize;
 
-    public SynchronizeIngestedDebtPositionActivityImpl(DebtPositionSearchService debtPositionSearchService) {
-        this.debtPositionSearchService = debtPositionSearchService;
+    private static final List<String> DEFAULT_ORDERING = List.of("debtPositionId","asc");
+
+    public SynchronizeIngestedDebtPositionActivityImpl(DebtPositionService debtPositionService,
+                                                       @Value("${query-limits.debt-positions.size}") Integer pageSize) {
+        this.debtPositionService = debtPositionService;
+        this.pageSize = pageSize;
     }
 
     @Override
     public String synchronizeIngestedDebtPosition(Long ingestionFlowFileId) {
         log.info("Synchronizing all debt positions related to ingestion flow file id {}", ingestionFlowFileId);
-        List<DebtPositionDTO> debtPositions = debtPositionSearchService.getAllDebtPositionsByIngestionFlowFileId(ingestionFlowFileId);
 
         StringBuilder errors = new StringBuilder();
-        debtPositions.forEach(debtPosition -> {
-            try {
-                // TODO invoke workflow sync and add any error (P4ADEV-2344)
-                // TODO invoke workflow status (P4ADEV-2345)
-            } catch (Exception e) {
-                log.error("Error synchronizing debt position with id {}: {}", debtPosition.getDebtPositionId(), e.getMessage());
-                errors.append("Error on debt position with iupdOrg " + debtPosition.getIupdOrg() + ": " + e.getMessage() + " \n");
+
+        int currentPage = 0;
+        boolean hasMorePages = true;
+
+        while (hasMorePages) {
+            PagedDebtPositions pagedDebtPositions = debtPositionService.getDebtPositionsByIngestionFlowFileId(ingestionFlowFileId,
+                    currentPage,
+                    pageSize,
+                    DEFAULT_ORDERING);
+
+            if(pagedDebtPositions == null){
+                throw new InvalidValueException("No debt positions found for the ingestion flow file with id " + ingestionFlowFileId);
             }
-        });
+
+            pagedDebtPositions.getContent().forEach(debtPosition -> {
+                try {
+                    // TODO invoke workflow sync and add any error (P4ADEV-2344)
+                    // TODO invoke workflow status (P4ADEV-2345)
+                } catch (Exception e) {
+                    log.error("Error synchronizing debt position with id {}: {}", debtPosition.getDebtPositionId(), e.getMessage());
+                    errors.append("Error on debt position with iupdOrg " + debtPosition.getIupdOrg() + ": " + e.getMessage() + " \n");
+                }
+            });
+
+            currentPage++;
+            hasMorePages = currentPage < pagedDebtPositions.getTotalPages();
+        }
 
         return errors.toString();
     }
--- src/test/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/debtposition/SynchronizeIngestedDebtPositionActivityTest.java
+++ src/test/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/debtposition/SynchronizeIngestedDebtPositionActivityTest.java
@@ -1,7 +1,9 @@
 package it.gov.pagopa.payhub.activities.activity.ingestionflow.debtposition;
 
-import it.gov.pagopa.payhub.activities.service.debtposition.DebtPositionSearchService;
+import it.gov.pagopa.payhub.activities.connector.debtposition.DebtPositionService;
+import it.gov.pagopa.payhub.activities.exception.InvalidValueException;
 import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionDTO;
+import it.gov.pagopa.pu.debtposition.dto.generated.PagedDebtPositions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,33 +15,69 @@ import java.util.List;
 
 import static it.gov.pagopa.payhub.activities.util.faker.DebtPositionFaker.buildDebtPositionDTO;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 class SynchronizeIngestedDebtPositionActivityTest {
 
     @Mock
-    private DebtPositionSearchService debtPositionSearchServiceMock;
+    private DebtPositionService debtPositionServiceMock;
+    private static final Integer PAGE_SIZE = 2;
+    private static final List<String> DEFAULT_ORDERING = List.of("debtPositionId","asc");
 
     private SynchronizeIngestedDebtPositionActivity activity;
 
     @BeforeEach
     void setUp() {
         activity = new SynchronizeIngestedDebtPositionActivityImpl(
-                debtPositionSearchServiceMock
+                debtPositionServiceMock, PAGE_SIZE
         );
     }
 
     @Test
     void testSynchronizeIngestedDebtPositionWithoutErrors(){
         Long ingestionFlowFileId = 1L;
-        List<DebtPositionDTO> debtPositions = List.of(buildDebtPositionDTO(), buildDebtPositionDTO());
+        DebtPositionDTO debtPosition1 = buildDebtPositionDTO();
+        DebtPositionDTO debtPosition2 = buildDebtPositionDTO();
+        DebtPositionDTO debtPosition3 = buildDebtPositionDTO();
+        DebtPositionDTO debtPosition4 = buildDebtPositionDTO();
 
-        Mockito.when(debtPositionSearchServiceMock.getAllDebtPositionsByIngestionFlowFileId(ingestionFlowFileId))
-                .thenReturn(debtPositions);
+        PagedDebtPositions pagedDebtPositionsFirstPage = PagedDebtPositions.builder()
+                .content(List.of(debtPosition1, debtPosition2))
+                .size(2L)
+                .totalPages(2L)
+                .totalElements(4L)
+                .number(0L)
+                .build();
+
+        PagedDebtPositions pagedDebtPositionsSecondPage = PagedDebtPositions.builder()
+                .content(List.of(debtPosition3, debtPosition4))
+                .size(2L)
+                .totalPages(2L)
+                .totalElements(4L)
+                .number(1L)
+                .build();
+
+        Mockito.when(debtPositionServiceMock.getDebtPositionsByIngestionFlowFileId(ingestionFlowFileId, 0, PAGE_SIZE, DEFAULT_ORDERING))
+                .thenReturn(pagedDebtPositionsFirstPage);
+        Mockito.when(debtPositionServiceMock.getDebtPositionsByIngestionFlowFileId(ingestionFlowFileId, 1, PAGE_SIZE, DEFAULT_ORDERING))
+                .thenReturn(pagedDebtPositionsSecondPage);
 
         String result = activity.synchronizeIngestedDebtPosition(ingestionFlowFileId);
 
         assertEquals("", result);
     }
 
+    @Test
+    void testSynchronizeIngestedDebtPositionWithNullPagedDebtPositionsRetrieved(){
+        Long ingestionFlowFileId = 1L;
+
+        Mockito.when(debtPositionServiceMock.getDebtPositionsByIngestionFlowFileId(ingestionFlowFileId, 0, PAGE_SIZE, DEFAULT_ORDERING))
+                .thenReturn(null);
+
+        InvalidValueException exception = assertThrows(InvalidValueException.class,
+                () -> activity.synchronizeIngestedDebtPosition(ingestionFlowFileId));
+
+        assertEquals("No debt positions found for the ingestion flow file with id 1", exception.getMessage());
+    }
 }
