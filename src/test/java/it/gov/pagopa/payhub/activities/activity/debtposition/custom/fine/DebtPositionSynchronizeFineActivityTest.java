327e277495a47001a618871ada588dd1f988b62c
LarissaASLeite <105739146+LarissaASLeite@users.noreply.github.com>
2025-03-25 11:54:04 +0100

    fix: P4ADEV-2443-validate-fine (#199)

---
+++ src/test/java/it/gov/pagopa/payhub/activities/activity/debtposition/custom/fine/DebtPositionSynchronizeFineActivityTest.java
@@ -0,0 +1,47 @@
+package it.gov.pagopa.payhub.activities.activity.debtposition.custom.fine;
+
+import it.gov.pagopa.payhub.activities.dto.IONotificationMessage;
+import it.gov.pagopa.payhub.activities.dto.debtposition.syncwfconfig.FineWfExecutionConfig;
+import it.gov.pagopa.payhub.activities.service.debtposition.custom.fine.DebtPositionFineValidation;
+import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static it.gov.pagopa.payhub.activities.util.faker.DebtPositionFaker.buildDebtPositionDTO;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class DebtPositionSynchronizeFineActivityTest {
+
+    @Mock
+    private DebtPositionFineValidation debtPositionFineValidationMock;
+
+    private DebtPositionSynchronizeFineActivity activity;
+
+    @BeforeEach
+    void setUp(){
+        activity = new DebtPositionSynchronizeFineActivityImpl(debtPositionFineValidationMock);
+    }
+
+    @Test
+    void whenHandleFineDebtPositionThenOk(){
+        //TODO to be fixed with task https://pagopa.atlassian.net/browse/P4ADEV-2442
+        DebtPositionDTO debtPositionDTO = buildDebtPositionDTO();
+        FineWfExecutionConfig fineWfExecutionConfig = buildfineWfExecutionConfig();
+
+        activity.handleFineDebtPosition(debtPositionDTO, false, fineWfExecutionConfig);
+
+        verify(debtPositionFineValidationMock).validateFine(debtPositionDTO);
+    }
+
+    private FineWfExecutionConfig buildfineWfExecutionConfig() {
+        IONotificationMessage notified = new IONotificationMessage("notified", "notified");
+        IONotificationMessage reductionExpired = new IONotificationMessage("reductionExpired", "reductionExpired");
+        return FineWfExecutionConfig.builder()
+                .ioMessages(new FineWfExecutionConfig.IONotificationFineWfMessages(notified, reductionExpired))
+                .build();
+    }
+}
