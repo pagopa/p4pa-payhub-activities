fc4fa4e63b41ad317d21aa6fd2025f3d845c5e57
dgiacobbe <domenico.giacobbe@eng.it>
2025-03-28 11:52:55 +0100

    P4ADEV-1997 interface for PaymentNotificationIngestionActivity

---
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/paymentnotification/PaymentNotificationIngestionActivity.java
@@ -0,0 +1,21 @@
+package it.gov.pagopa.payhub.activities.activity.ingestionflow.paymentnotification;
+
+import io.temporal.activity.ActivityInterface;
+import io.temporal.activity.ActivityMethod;
+import it.gov.pagopa.payhub.activities.dto.paymentnotification.PaymentNotificationIngestionFlowFileActivityResult;
+/**
+ * Interface for the PaymentNotificationIngestionActivity.
+ * Defines methods for processing payment notification files based on an IngestionFlowFile ID.
+ */
+@ActivityInterface
+public interface PaymentNotificationIngestionActivity {
+
+  /**
+   * Processes a payment notification file based on the provided IngestionFlowFile ID.
+   *
+   * @param ingestionFlowFileId the unique identifier related to the file to process.
+   * @return {@link PaymentNotificationIngestionFlowFileActivityResult} containing the list of IUDs and organization Id.
+   */
+  @ActivityMethod
+  PaymentNotificationIngestionFlowFileActivityResult processFile(Long ingestionFlowFileId);
+}
