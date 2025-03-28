209385fac890eb98bda132a9bbaf6f3ccf987c0c
LarissaASLeite <105739146+LarissaASLeite@users.noreply.github.com>
2025-03-28 12:01:00 +0100

    fix: P4ADEV-2444-update-fine-activity (#210)

--- src/test/java/it/gov/pagopa/payhub/activities/activity/debtposition/custom/fine/DebtPositionSynchronizeFineActivityTest.java
+++ src/test/java/it/gov/pagopa/payhub/activities/activity/debtposition/custom/fine/DebtPositionSynchronizeFineActivityTest.java
@@ -1,16 +1,23 @@
 package it.gov.pagopa.payhub.activities.activity.debtposition.custom.fine;
 
 import it.gov.pagopa.payhub.activities.dto.IONotificationMessage;
+import it.gov.pagopa.payhub.activities.dto.debtposition.HandleFineDebtPositionResult;
 import it.gov.pagopa.payhub.activities.dto.debtposition.syncwfconfig.FineWfExecutionConfig;
+import it.gov.pagopa.payhub.activities.service.debtposition.custom.fine.DebtPositionFineNotificationDateProcessor;
+import it.gov.pagopa.payhub.activities.service.debtposition.custom.fine.DebtPositionFineProcessor;
 import it.gov.pagopa.payhub.activities.service.debtposition.custom.fine.DebtPositionFineValidation;
 import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.OffsetDateTime;
+
 import static it.gov.pagopa.payhub.activities.util.faker.DebtPositionFaker.buildDebtPositionDTO;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -18,23 +25,35 @@ class DebtPositionSynchronizeFineActivityTest {
 
     @Mock
     private DebtPositionFineValidation debtPositionFineValidationMock;
+    @Mock
+    private DebtPositionFineNotificationDateProcessor notificationDateProcessorMock;
+    @Mock
+    private DebtPositionFineProcessor debtPositionFineProcessorMock;
 
     private DebtPositionSynchronizeFineActivity activity;
 
     @BeforeEach
     void setUp(){
-        activity = new DebtPositionSynchronizeFineActivityImpl(debtPositionFineValidationMock);
+        activity = new DebtPositionSynchronizeFineActivityImpl(debtPositionFineValidationMock, notificationDateProcessorMock, debtPositionFineProcessorMock);
     }
 
     @Test
     void whenHandleFineDebtPositionThenOk(){
-        //TODO to be fixed with task https://pagopa.atlassian.net/browse/P4ADEV-2442
+        // Given
         DebtPositionDTO debtPositionDTO = buildDebtPositionDTO();
         FineWfExecutionConfig fineWfExecutionConfig = buildfineWfExecutionConfig();
 
-        activity.handleFineDebtPosition(debtPositionDTO, false, fineWfExecutionConfig);
+        HandleFineDebtPositionResult expectedResult = new HandleFineDebtPositionResult(debtPositionDTO, OffsetDateTime.now().plusDays(2), true);
+        Mockito.when(notificationDateProcessorMock.processNotificationDate(debtPositionDTO, fineWfExecutionConfig))
+                .thenReturn(expectedResult);
 
+        // When
+        HandleFineDebtPositionResult result = activity.handleFineDebtPosition(debtPositionDTO, false, fineWfExecutionConfig);
+
+        // Then
         verify(debtPositionFineValidationMock).validateFine(debtPositionDTO);
+        verify(debtPositionFineProcessorMock).processFine(expectedResult);
+        assertEquals(expectedResult, result);
     }
 
     private FineWfExecutionConfig buildfineWfExecutionConfig() {
