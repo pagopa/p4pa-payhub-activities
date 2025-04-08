fd3c83bb917e0f5e63f74566ce7fae0e4f33bea1
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-04-08 11:59:30 +0200

    P4ADEV-2626 added QueryParamsPlusEncoderInterceptor

---
+++ src/test/java/it/gov/pagopa/payhub/activities/config/interceptor/QueryParamsPlusEncoderInterceptorTest.java
@@ -0,0 +1,66 @@
+package it.gov.pagopa.payhub.activities.config.interceptor;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+import java.net.URI;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class QueryParamsPlusEncoderInterceptorTest {
+
+    @Mock
+    private HttpRequest mockRequest;
+    @Mock
+    private ClientHttpRequestExecution mockExecution;
+
+    byte[] mockBody = new byte[0];
+
+    private QueryParamsPlusEncoderInterceptor queryParamsPlusEncoderInterceptor;
+
+    @BeforeEach
+    void setUp() {
+        queryParamsPlusEncoderInterceptor = new QueryParamsPlusEncoderInterceptor();
+    }
+
+    @Test
+    void givenWhenInterceptThen() throws IOException {
+        //given
+        ClientHttpResponse mockResponse = mock(ClientHttpResponse.class);
+        OffsetDateTime offsetDateTime = OffsetDateTime.now();
+        String formattedDateTime = offsetDateTime.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        URI uri = URI.create("http://example/api/resource?datetime=" + formattedDateTime);
+
+        when(mockRequest.getURI()).thenReturn(uri);
+        when(mockExecution.execute(Mockito.any(), Mockito.eq(mockBody))).thenReturn(mockResponse);
+
+        //when
+        ClientHttpResponse actualResponse = queryParamsPlusEncoderInterceptor.intercept(mockRequest, mockBody, mockExecution);
+
+        //then
+        assertEquals(mockResponse, actualResponse); // Ensure the execution continues
+
+        ArgumentCaptor<HttpRequest> requestCaptor = ArgumentCaptor.forClass(HttpRequest.class);
+        Mockito.verify(mockExecution).execute(requestCaptor.capture(), Mockito.eq(mockBody));
+        HttpRequest interceptedRequest = requestCaptor.getValue();
+
+        // Verify the URI of the intercepted request
+        URI interceptedUri = interceptedRequest.getURI();
+        assertEquals("http://example/api/resource?datetime=2025-04-08T11:57:03.3752754%2B02:00", interceptedUri.toString());
+    }
+
+}
