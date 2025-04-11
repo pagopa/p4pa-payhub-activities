66f24aa4e7bd010ff5b294b873ee867e38341183
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2025-04-11 12:55:40 +0200

    fix: IngestionFlowFileResult toString (#237)

--- src/main/java/it/gov/pagopa/payhub/activities/dto/ingestion/paymentsreporting/PaymentsReportingIngestionFlowFileActivityResult.java
+++ src/main/java/it/gov/pagopa/payhub/activities/dto/ingestion/paymentsreporting/PaymentsReportingIngestionFlowFileActivityResult.java
@@ -2,10 +2,7 @@ package it.gov.pagopa.payhub.activities.dto.ingestion.paymentsreporting;
 
 import it.gov.pagopa.payhub.activities.dto.classifications.PaymentsReportingTransferDTO;
 import it.gov.pagopa.payhub.activities.dto.ingestion.IngestionFlowFileResult;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
@@ -15,6 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
+@ToString(callSuper = true)
 public class PaymentsReportingIngestionFlowFileActivityResult extends IngestionFlowFileResult {
     private String iuf;
     private Long organizationId;
