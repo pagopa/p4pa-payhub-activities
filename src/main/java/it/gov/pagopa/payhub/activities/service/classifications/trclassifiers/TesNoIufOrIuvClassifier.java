5a140bf760a0e8648238099907a705d62f5680ce
macacia <marco.cacia@eng.it>
2025-04-18 17:28:22 +0200

    TesNoIufOrIuvClassifier

---
+++ src/main/java/it/gov/pagopa/payhub/activities/service/classifications/trclassifiers/TesNoIufOrIuvClassifier.java
@@ -0,0 +1,20 @@
+package it.gov.pagopa.payhub.activities.service.classifications.trclassifiers;
+
+import it.gov.pagopa.payhub.activities.dto.treasury.TreasuryIuf;
+import it.gov.pagopa.pu.classification.dto.generated.ClassificationsEnum;
+import it.gov.pagopa.pu.classification.dto.generated.PaymentNotificationNoPII;
+import it.gov.pagopa.pu.classification.dto.generated.PaymentsReporting;
+import it.gov.pagopa.pu.debtposition.dto.generated.Transfer;
+
+public class TesNoIufOrIuvClassifier implements TransferClassifier {
+
+	@Override
+	public ClassificationsEnum classify(Transfer transferDTO, PaymentNotificationNoPII paymentNotificationDTO, PaymentsReporting paymentsReportingDTO, TreasuryIuf treasuryIuf) {
+		if (transferDTO == null && paymentNotificationDTO == null && paymentsReportingDTO == null && treasuryIuf != null &&
+			(treasuryIuf.getIuf() == null || treasuryIuf.getIuv() == null)
+		) {
+			return ClassificationsEnum.TES_NO_IUF_OR_IUV;
+		}
+		return null;
+	}
+}
---
+++ src/test/java/it/gov/pagopa/payhub/activities/service/classifications/trclassifiers/TesNoIufOrIuvClassifierTest.java
@@ -0,0 +1,37 @@
+package it.gov.pagopa.payhub.activities.service.classifications.trclassifiers;
+
+import it.gov.pagopa.payhub.activities.dto.treasury.TreasuryIuf;
+import it.gov.pagopa.pu.classification.dto.generated.ClassificationsEnum;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TesNoIufOrIuvClassifierTest {
+	private TesNoIufOrIuvClassifier classifier = new TesNoIufOrIuvClassifier();
+
+	@ParameterizedTest
+	@MethodSource("provideClassifierScenarios")
+	void classificationCoversAllCombinations(TreasuryIuf treasury, ClassificationsEnum expected) {
+		ClassificationsEnum result = classifier.classify(null, null, null, treasury);
+		assertEquals(expected, result);
+	}
+
+	private static Stream<Arguments> provideClassifierScenarios() {
+		TreasuryIuf treasuryWithIuf = TreasuryIuf.builder().iuf("iuf").build();
+		TreasuryIuf treasuryWithIuv = TreasuryIuf.builder().iuv("iuv").build();
+		TreasuryIuf treasuryWithIufIuv = TreasuryIuf.builder().iuf("iuf").iuv("iuv").build();
+		return Stream.of(
+			Arguments.of(null, null),
+			Arguments.of(treasuryWithIuf, ClassificationsEnum.TES_NO_IUF_OR_IUV),
+			Arguments.of(treasuryWithIuv, ClassificationsEnum.TES_NO_IUF_OR_IUV),
+			Arguments.of(treasuryWithIufIuv, null)
+		);
+	}
+}
