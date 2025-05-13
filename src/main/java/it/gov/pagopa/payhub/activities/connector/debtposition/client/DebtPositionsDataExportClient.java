6fa9695707ce380653e6f9a6471b394b5fad98b9
pelliccm <matteo.pelliccione@nttdata.com>
2025-05-13 16:28:11 +0200

    [P4ADEV-2649] update create-export-file date filters

--- src/main/java/it/gov/pagopa/payhub/activities/connector/debtposition/client/DebtPositionsDataExportClient.java
+++ src/main/java/it/gov/pagopa/payhub/activities/connector/debtposition/client/DebtPositionsDataExportClient.java
@@ -1,18 +1,16 @@
 package it.gov.pagopa.payhub.activities.connector.debtposition.client;
 
 import it.gov.pagopa.payhub.activities.connector.debtposition.config.DebtPositionApisHolder;
-import it.gov.pagopa.payhub.activities.util.Utilities;
 import it.gov.pagopa.pu.debtposition.dto.generated.PagedInstallmentsPaidView;
 import it.gov.pagopa.pu.debtposition.dto.generated.PagedReceiptsArchivingView;
-import it.gov.pagopa.pu.processexecutions.dto.generated.LocalDateIntervalFilter;
+import it.gov.pagopa.pu.processexecutions.dto.generated.OffsetDateTimeIntervalFilter;
 import it.gov.pagopa.pu.processexecutions.dto.generated.PaidExportFileFilter;
 import it.gov.pagopa.pu.processexecutions.dto.generated.ReceiptsArchivingExportFileFilter;
+import java.time.OffsetDateTime;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
-
-import java.time.OffsetDateTime;
-import java.util.List;
 
 @Lazy
 @Slf4j
@@ -26,17 +24,17 @@ public class DebtPositionsDataExportClient {
     }
 
     public PagedInstallmentsPaidView getExportPaidInstallments(String accessToken, Long organizationId, String operatorExternalUserId, PaidExportFileFilter paidExportFileFilter, Integer page, Integer size, List<String> sort) {
-        LocalDateIntervalFilter paymentDate = paidExportFileFilter.getPaymentDate();
-        OffsetDateTime from = paymentDate != null ? Utilities.toOffsetDateTimeStartOfTheDay(paymentDate.getFrom()): null;
-        OffsetDateTime to = paymentDate != null ? Utilities.toOffsetDateTimeEndOfTheDay(paymentDate.getTo()): null;
+        OffsetDateTimeIntervalFilter paymentDateTime = paidExportFileFilter.getPaymentDateTime();
+        OffsetDateTime from = paymentDateTime != null ? paymentDateTime.getFrom() : null;
+        OffsetDateTime to = paymentDateTime != null ? paymentDateTime.getTo() : null;
 
         return debtPositionApisHolder.getDataExportsApi(accessToken).exportPaidInstallments(organizationId, operatorExternalUserId, from, to, paidExportFileFilter.getDebtPositionTypeOrgId(), page, size, sort);
     }
 
     public PagedReceiptsArchivingView getExportReceiptsArchivingView(String accessToken, Long organizationId, String operatorExternalUserId, ReceiptsArchivingExportFileFilter receiptsArchivingExportFileFilter, Integer page, Integer size, List<String> sort) {
-        LocalDateIntervalFilter paymentDate = receiptsArchivingExportFileFilter.getPaymentDate();
-        OffsetDateTime from = paymentDate != null ? Utilities.toOffsetDateTimeStartOfTheDay(paymentDate.getFrom()): null;
-        OffsetDateTime to = paymentDate != null ? Utilities.toOffsetDateTimeEndOfTheDay(paymentDate.getTo()): null;
+        OffsetDateTimeIntervalFilter paymentDateTime = receiptsArchivingExportFileFilter.getPaymentDateTime();
+        OffsetDateTime from = paymentDateTime != null ? paymentDateTime.getFrom() : null;
+        OffsetDateTime to = paymentDateTime != null ? paymentDateTime.getTo() : null;
 
         return debtPositionApisHolder.getDataExportsApi(accessToken).exportArchivingReceipts(organizationId, operatorExternalUserId, from, to, page, size, sort);
     }
