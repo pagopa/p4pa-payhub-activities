77e01791f41cd4f6e019666eb06a1c06eb5894cf
domenicogi <domenico.giacobbe@eng.it>
2025-05-21 15:14:29 +0200

    feat: P4ADEV-2968 save encrypt api keys (#283)
    Co-authored-by: antonioT90 <34568575+antonioT90@users.noreply.github.com>

---
+++ src/main/java/it/gov/pagopa/payhub/activities/connector/organization/OrganizationApiServiceImpl.java
@@ -0,0 +1,23 @@
+package it.gov.pagopa.payhub.activities.connector.organization;
+
+import it.gov.pagopa.payhub.activities.connector.auth.AuthnService;
+import it.gov.pagopa.payhub.activities.connector.organization.client.OrganizationApiClient;
+import it.gov.pagopa.pu.organization.dto.generated.OrganizationApiKeys;
+
+public class OrganizationApiServiceImpl implements OrganizationApiService {
+
+  private final AuthnService authnService;
+  private final OrganizationApiClient organizationApiClient;
+
+  public OrganizationApiServiceImpl(AuthnService authnService,
+      OrganizationApiClient organizationApiClient) {
+    this.authnService = authnService;
+    this.organizationApiClient = organizationApiClient;
+  }
+
+  @Override
+  public void encryptAndSaveApiKey(Long organizationId, OrganizationApiKeys organizationApiKeys) {
+    organizationApiClient.encryptAndSaveApiKey(organizationId, organizationApiKeys,
+        authnService.getAccessToken());
+  }
+}
