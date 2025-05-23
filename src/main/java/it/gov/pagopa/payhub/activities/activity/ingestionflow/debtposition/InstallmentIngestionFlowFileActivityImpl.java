6fc44c3c574652b593c3a81f307e04de27b7aa03
LarissaASLeite <larissaaiala.silvaleite.st@nttdata.com>
2025-05-23 16:10:03 +0200

    fix-versions-for-installment-ingestion-flow-file

--- src/main/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/debtposition/InstallmentIngestionFlowFileActivityImpl.java
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/debtposition/InstallmentIngestionFlowFileActivityImpl.java
@@ -60,12 +60,14 @@ public class InstallmentIngestionFlowFileActivityImpl extends BaseIngestionFlowF
         log.info("Processing file: {}", filePath);
 
         try {
-            return csvService.readCsv(filePath, InstallmentIngestionFlowFileDTO.class, (csvIterator, readerExceptions) ->
-                    installmentProcessingService.processInstallments(csvIterator, readerExceptions, ingestionFlowFileDTO, workingDirectory),
-                    ingestionFlowFileDTO.getFileVersion());
+            InstallmentIngestionFlowFileResult result = csvService.readCsv(filePath, InstallmentIngestionFlowFileDTO.class, (csvIterator, readerExceptions) ->
+                    installmentProcessingService.processInstallments(csvIterator, readerExceptions, ingestionFlowFileDTO, workingDirectory));
+
+            result.setOrganizationId(ingestionFlowFileDTO.getOrganizationId());
+            return result;
         } catch (Exception e) {
-            log.error("Error processing file {} with version {}: {}", filePath, ingestionFlowFileDTO.getFileVersion(), e.getMessage(), e);
-            throw new InvalidIngestionFileException(String.format("Error processing file %s with version %s: %s", filePath, ingestionFlowFileDTO.getFileVersion(), e.getMessage()));
+            log.error("Error processing file {}: {}", filePath, e.getMessage(), e);
+            throw new InvalidIngestionFileException(String.format("Error processing file %s: %s", filePath, e.getMessage()));
         }
     }
 }
