ae6588a96f0670c47acac4944b13dc3c8a7efed9
domenicogi <domenico.giacobbe@eng.it>
2025-05-23 18:05:34 +0200

    feat: P4ADEV-2996 debtpositiontype activity (#292)
    Co-authored-by: antonioT90 <34568575+antonioT90@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/payhub/activities/connector/debtposition/DebtPositionTypeService.java
+++ src/main/java/it/gov/pagopa/payhub/activities/connector/debtposition/DebtPositionTypeService.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.payhub.activities.connector.debtposition;
 
+import it.gov.pagopa.pu.debtposition.dto.generated.CollectionModelDebtPositionType;
 import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionType;
 import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionTypeRequestBody;
 
@@ -15,4 +16,19 @@ public interface DebtPositionTypeService {
      */
     DebtPositionType createDebtPositionType(DebtPositionTypeRequestBody debtPositionTypeRequestBody);
 
+
+    /**
+     * Retrieves a collection of debt position types filtered by the specified main fields.
+     *
+     * @param code the code of the debt position type
+     * @param brokerId the broker identifier
+     * @param orgType the organization type
+     * @param macroArea the macro area
+     * @param serviceType the service type
+     * @param collectingReason the collecting reason
+     * @param taxonomyCode the taxonomy code
+     * @return a collection of matching DebtPositionType objects
+     */
+    CollectionModelDebtPositionType getByMainFields(String code, Long brokerId, String orgType,
+        String macroArea, String serviceType, String collectingReason, String taxonomyCode);
 }
