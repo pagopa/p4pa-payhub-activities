77fcda3305720ac19803587c391da74ab935fe65
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2025-06-04 09:55:31 +0200

    P4ADEV-2597 added service

---
+++ src/main/java/it/gov/pagopa/payhub/activities/service/exportflow/email/ExportFileEmailContentConfigurerService.java
@@ -0,0 +1,73 @@
+package it.gov.pagopa.payhub.activities.service.exportflow.email;
+
+import it.gov.pagopa.payhub.activities.config.EmailTemplatesConfiguration;
+import it.gov.pagopa.pu.organization.dto.generated.Organization;
+import it.gov.pagopa.pu.processexecutions.dto.generated.ExportFile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Lazy
+@Service
+public class ExportFileEmailContentConfigurerService {
+
+    private static final DateTimeFormatter MAILDATETIMEFORMATTER = DateTimeFormatter.ofPattern("EEE, MMM dd yyyy, hh:mm:ss");
+
+    private final EmailTemplatesConfiguration emailTemplatesConfiguration;
+
+    private final String exportUrl;
+    private final String env;
+
+    private static final Map<ExportFile.ExportFileTypeEnum, String> EXPORT_FILE_TYPE_MAP = Map.of(
+            ExportFile.ExportFileTypeEnum.PAID, "pagati",
+            ExportFile.ExportFileTypeEnum.RECEIPTS_ARCHIVING, "conservazione",
+            ExportFile.ExportFileTypeEnum.CLASSIFICATIONS, "classificazione"
+    );
+
+
+    public ExportFileEmailContentConfigurerService(EmailTemplatesConfiguration emailTemplatesConfiguration,
+                                                   @Value("${export-flow-files.mail.request-url}") String exportUrl,
+                                                   @Value("${export-flow-files.mail.env}") String env) {
+        this.emailTemplatesConfiguration = emailTemplatesConfiguration;
+        this.exportUrl = exportUrl;
+        this.env = env;
+    }
+
+
+    public Map<String, String> configureParams(ExportFile exportFile, Organization organization, boolean success) {
+        return getMailParameters(exportFile.getFileName(), organization.getOrgName(),exportFile.getExportFileType(), success);
+    }
+
+
+    private Map<String, String> getMailParameters(String fileName, String orgName, ExportFile.ExportFileTypeEnum exportFileTypeEnum, boolean success) {
+        Map<String, String> mailParams = new HashMap<>();
+        String mailText;
+        if (success) {
+            mailText = emailTemplatesConfiguration.getExportMailTextOk();
+            mailParams.put("exportUrl", exportUrl);
+            mailParams.put("filename", fileName);
+
+        } else {
+            mailText = emailTemplatesConfiguration.getExportMailTextKo();
+        }
+        mailParams.put("exportFileType", getExportFileType(exportFileTypeEnum));
+        mailParams.put("entityName", orgName);
+        mailParams.put("env", env);
+        mailParams.put("mailText", mailText);
+        mailParams.put("currentDate", MAILDATETIMEFORMATTER.format(LocalDateTime.now()));
+
+        return mailParams;
+    }
+
+
+    private String getExportFileType(ExportFile.ExportFileTypeEnum exportFileTypeEnum) {
+        return EXPORT_FILE_TYPE_MAP.getOrDefault(exportFileTypeEnum, null);
+    }
+
+
+}
