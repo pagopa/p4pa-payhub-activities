cc8d33e6816f068f2887f88ccf3e0edaa39e1524
Antonio Calò <antonio.calo@nttdata.com>
2025-06-19 15:33:14 +0200

    feat: P4ADEV-3214 handle source flow name to map2dummyreceipt (#324)

--- src/main/java/it/gov/pagopa/payhub/activities/service/pagopapayments/GenerateNoticeService.java
+++ src/main/java/it/gov/pagopa/payhub/activities/service/pagopapayments/GenerateNoticeService.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.payhub.activities.service.pagopapayments;
 import it.gov.pagopa.payhub.activities.connector.pagopapayments.PrintPaymentNoticeService;
 import it.gov.pagopa.payhub.activities.connector.processexecutions.IngestionFlowFileService;
 import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionDTO;
+import it.gov.pagopa.pu.debtposition.dto.generated.InstallmentStatus;
 import it.gov.pagopa.pu.pagopapayments.dto.generated.GeneratedNoticeMassiveFolderDTO;
 import it.gov.pagopa.pu.pagopapayments.dto.generated.NoticeRequestMassiveDTO;
 import lombok.extern.slf4j.Slf4j;
@@ -23,21 +24,27 @@ public class GenerateNoticeService {
         this.ingestionFlowFileService = ingestionFlowFileService;
     }
 
-    public String generateNotices(Long ingestionFlowFileId, List<DebtPositionDTO> debtPositionsGenerateNotices) {
-        String requestId = "PU_" + debtPositionsGenerateNotices.getFirst().getOrganizationId() + "_" + ingestionFlowFileId;
-        log.info("Triggering notice generateNotices for {} debtPositions with requestId {}", debtPositionsGenerateNotices.size(), requestId);
+  public String generateNotices(Long ingestionFlowFileId, List<DebtPositionDTO> debtPositionsGenerateNotices) {
+    String requestId = "PU_" + debtPositionsGenerateNotices.getFirst().getOrganizationId() + "_" + ingestionFlowFileId;
+    log.info("Triggering notice generateNotices for {} debtPositions with requestId {}", debtPositionsGenerateNotices.size(), requestId);
 
-        NoticeRequestMassiveDTO request = NoticeRequestMassiveDTO.builder()
-                .debtPositions(debtPositionsGenerateNotices)
-                .requestId(requestId)
-                .build();
-        GeneratedNoticeMassiveFolderDTO folderDTO = printPaymentNoticeService.generateMassive(request);
+    NoticeRequestMassiveDTO request = NoticeRequestMassiveDTO.builder()
+        .debtPositions(debtPositionsGenerateNotices)
+        .requestId(requestId)
+        .build();
+    GeneratedNoticeMassiveFolderDTO folderDTO = printPaymentNoticeService.generateMassive(request);
 
-        ingestionFlowFileService.updatePdfGenerated(
-                ingestionFlowFileId,
-                (long) debtPositionsGenerateNotices.size(),
-                folderDTO.getFolderId()
-        );
-        return folderDTO.getFolderId();
-    }
+    long pdfGenerated = debtPositionsGenerateNotices.stream()
+        .flatMap(debt -> debt.getPaymentOptions().stream())
+        .flatMap(option -> option.getInstallments().stream())
+        .filter(installment -> installment.getStatus() == InstallmentStatus.UNPAID)
+        .count();
+
+    ingestionFlowFileService.updatePdfGenerated(
+        ingestionFlowFileId,
+        pdfGenerated,
+        folderDTO.getFolderId()
+    );
+    return folderDTO.getFolderId();
+  }
 }
