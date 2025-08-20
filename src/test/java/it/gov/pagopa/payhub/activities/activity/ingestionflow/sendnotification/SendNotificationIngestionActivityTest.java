e9825fc93368a50760d966d2e3d94bc23adcac33
antoniocalo <antonio.calo@nttdata.com>
2025-08-20 11:50:24 +0200

    P4ADEV-3329 add test

--- src/test/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/sendnotification/SendNotificationIngestionActivityTest.java
+++ src/test/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/sendnotification/SendNotificationIngestionActivityTest.java
@@ -38,7 +38,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.RestClientException;
 
 @ExtendWith(MockitoExtension.class)
-class SendNotificationIngestionActivityImplTest {
+class SendNotificationIngestionActivityTest {
 
   @Mock
   private CsvService csvServiceMock;
