1f8da3be2bcbe394400bc74f8610ff8805280d5c
antoniocalo <antonio.calo@nttdata.com>
2025-08-20 11:46:26 +0200

    P4ADEV-3329 add test

--- src/test/java/it/gov/pagopa/payhub/activities/connector/sendnotification/SendNotificationServiceTest.java
+++ src/test/java/it/gov/pagopa/payhub/activities/connector/sendnotification/SendNotificationServiceTest.java
@@ -2,6 +2,8 @@ package it.gov.pagopa.payhub.activities.connector.sendnotification;
 
 import it.gov.pagopa.payhub.activities.connector.auth.AuthnService;
 import it.gov.pagopa.payhub.activities.connector.sendnotification.client.SendNotificationClient;
+import it.gov.pagopa.pu.sendnotification.dto.generated.CreateNotificationRequest;
+import it.gov.pagopa.pu.sendnotification.dto.generated.CreateNotificationResponse;
 import it.gov.pagopa.pu.sendnotification.dto.generated.SendNotificationDTO;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -33,7 +35,7 @@ class SendNotificationServiceTest {
     }
 
     @Test
-    void givenSendNotificationIdWhenPreloadSendFileThenOk() {
+    void givenSendNotificationIdWhenGetSendNotificationThenOk() {
         // Given
         String accessToken = "ACCESSTOKEN";
         String sendNotificationId = "sendNotificationId";
@@ -46,6 +48,26 @@ class SendNotificationServiceTest {
 
         // When
         SendNotificationDTO result = service.getSendNotification(sendNotificationId);
+
+        // Then
+        Assertions.assertSame(expectedResult, result);
+    }
+
+    @Test
+    void givenCreateNotificationRequestWhenCreateSendNotificationThenOk() {
+        // Given
+        String accessToken = "ACCESSTOKEN";
+
+        CreateNotificationRequest request = new CreateNotificationRequest();
+        CreateNotificationResponse expectedResult = new CreateNotificationResponse();
+
+        Mockito.when(authnServiceMock.getAccessToken())
+            .thenReturn(accessToken);
+        Mockito.when(clientMock.createSendNotification(request, accessToken))
+            .thenReturn(expectedResult);
+
+        // When
+        CreateNotificationResponse result = service.createSendNotification(request);
 
         // Then
         Assertions.assertSame(expectedResult, result);
