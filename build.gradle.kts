917ec3e27084f77db1db514c7be53ab0cd2dcc0e
Benedetta-fabbri <124246048+Benedetta-fabbri@users.noreply.github.com>
2025-08-25 15:15:12 +0200

    fix: P4ADEV-3687 fix CVE-2025-48989 (#384)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.3"
+	id("org.springframework.boot") version "3.5.5"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.sonarqube") version "6.2.0.5505"
 	`java-library`
