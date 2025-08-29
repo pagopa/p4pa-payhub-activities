c4ec4b75f0ab224ab88c16db808b57373ac8e04a
dan-ntt <danbota1@emeal.nttdata.com>
2025-08-29 12:30:06 +0200

    feat: P4ADEV-3626 implemented the activity to delete installments on pagopa (#391)
    Co-authored-by: Dan Bota <dan.bota@nttdata.com>

---
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/receipt/DeletePaidInstallmentsOnPagoPaActivity.java
@@ -0,0 +1,18 @@
+package it.gov.pagopa.payhub.activities.activity.ingestionflow.receipt;
+
+import io.temporal.activity.ActivityInterface;
+import io.temporal.activity.ActivityMethod;
+import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionDTO;
+
+@ActivityInterface
+public interface DeletePaidInstallmentsOnPagoPaActivity {
+
+    /**
+     * Deletes paid installments based on debt position and receipt id
+     * @param debtPositionDTO the debt position
+     * @param receiptId the receipt id
+     */
+    @ActivityMethod
+    void deletePaidInstallmentsOnPagoPa(DebtPositionDTO debtPositionDTO, Long receiptId);
+
+}
