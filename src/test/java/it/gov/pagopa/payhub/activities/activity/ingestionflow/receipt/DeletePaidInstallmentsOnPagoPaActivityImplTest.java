777249e152adc35e8191abb345e4e5b1712a03aa
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2025-09-02 14:37:37 +0200

    P4ADEV-3742 add check on delete installment on ACA

--- src/test/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/receipt/DeletePaidInstallmentsOnPagoPaActivityImplTest.java
+++ src/test/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/receipt/DeletePaidInstallmentsOnPagoPaActivityImplTest.java
@@ -299,6 +299,36 @@ class DeletePaidInstallmentsOnPagoPaActivityImplTest {
         verify(gpdServiceMock, never()).syncInstallmentGpd(anyString(), any());
     }
 
+    @Test
+    void givenSyncWhenDeletingThenDoNothing() {
+        // Given
+        DebtPositionDTO debtPositionDTO = buildDebtPositionDTO();
+        ReceiptDTO receiptDTO = new ReceiptDTO();
+        receiptDTO.setReceiptId(1L);
+        Organization organization = new Organization();
+        organization.setBrokerId(1L);
+        Broker broker = new Broker();
+        broker.setPagoPaInteractionModel(PagoPaInteractionModel.SYNC);
+
+        InstallmentDTO installmentDTO = debtPositionDTO.getPaymentOptions().getFirst().getInstallments().getFirst();
+        installmentDTO.setSyncStatus(InstallmentSyncStatus.builder().syncStatusFrom(InstallmentStatus.PAID).syncStatusTo(InstallmentStatus.CANCELLED).build());
+        installmentDTO.setStatus(InstallmentStatus.TO_SYNC);
+        installmentDTO.setReceiptId(1L);
+
+        // When
+        when(receiptServiceMock.getByReceiptId(1L)).thenReturn(receiptDTO);
+        when(organizationServiceMock.getOrganizationById(debtPositionDTO.getOrganizationId())).thenReturn(Optional.of(organization));
+        when(brokerServiceMock.getBrokerById(1L)).thenReturn(broker);
+        activity.deletePaidInstallmentsOnPagoPa(debtPositionDTO, 1L);
+
+        // Then
+        verify(receiptServiceMock, times(1)).getByReceiptId(1L);
+        verify(organizationServiceMock, times(1)).getOrganizationById(debtPositionDTO.getOrganizationId());
+        verify(brokerServiceMock, times(1)).getBrokerById(1L);
+        verify(acaServiceMock, never()).syncInstallmentAca(anyString(), any());
+        verify(gpdServiceMock, never()).syncInstallmentGpd(anyString(), any());
+    }
+
 //    @Test
 //    void givenGpdWhenDeletingThenSyncOnGPD() {
 //        // Given
