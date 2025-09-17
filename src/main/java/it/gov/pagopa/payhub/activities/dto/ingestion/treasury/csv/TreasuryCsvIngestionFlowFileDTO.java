3fdaa372f3a31a57a98a6bb0746c2092042434f0
michelemazzini <113985823+michelemazzini@users.noreply.github.com>
2025-09-17 16:55:40 +0200

    feat: P4ADEV-3793 create treasury csv ingestion activity (#408)

---
+++ src/main/java/it/gov/pagopa/payhub/activities/dto/ingestion/treasury/csv/TreasuryCsvIngestionFlowFileDTO.java
@@ -0,0 +1,44 @@
+package it.gov.pagopa.payhub.activities.dto.ingestion.treasury.csv;
+
+import com.opencsv.bean.CsvBindByName;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TreasuryCsvIngestionFlowFileDTO {
+
+    @Pattern(regexp = "^\\d{4}$", message = "The year must be 4 digits long")
+    @CsvBindByName(column = "esercizio", required = true)
+    private String billYear;
+
+    @Size(max=7)
+    @CsvBindByName(column = "n_provvisorio", required = true)
+    private String billCode;
+
+    @Size(max=10)
+    @CsvBindByName(column = "data_esecuzione", required = true)
+    private String billDate;
+
+    @Size(max=255)
+    @CsvBindByName(column = "anagrafica_cliente", required = true)
+    private String pspLastName;
+
+    @NotNull
+    @Size(max=255)
+    @CsvBindByName(column = "descrizione_causale", required = true)
+    private String remittanceDescription;
+
+    @CsvBindByName(column = "importo_provvisorio", required = true)
+    private String billAmount;
+
+    @CsvBindByName(column = "valuta_ente")
+    private String regionValueDate;
+}
---
+++ src/main/java/it/gov/pagopa/payhub/activities/mapper/ingestionflow/treasury/csv/TreasuryCsvMapper.java
@@ -0,0 +1,46 @@
+package it.gov.pagopa.payhub.activities.mapper.ingestionflow.treasury.csv;
+
+import it.gov.pagopa.payhub.activities.dto.ingestion.treasury.csv.TreasuryCsvIngestionFlowFileDTO;
+import it.gov.pagopa.payhub.activities.util.TreasuryUtils;
+import it.gov.pagopa.pu.classification.dto.generated.Treasury;
+import it.gov.pagopa.pu.classification.dto.generated.TreasuryOrigin;
+import it.gov.pagopa.pu.processexecutions.dto.generated.IngestionFlowFile;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static it.gov.pagopa.payhub.activities.service.ingestionflow.treasury.TreasuryVersionBaseHandlerService.ORG_BT_CODE_DEFAULT;
+import static it.gov.pagopa.payhub.activities.service.ingestionflow.treasury.TreasuryVersionBaseHandlerService.ORG_ISTAT_CODE_DEFAULT;
+
+@Service
+public class TreasuryCsvMapper {
+
+    public Treasury map(TreasuryCsvIngestionFlowFileDTO dto, IngestionFlowFile ingestionFlowFile) {
+        LocalDate billDate = LocalDate.parse(dto.getBillDate(), DateTimeFormatter.ISO_LOCAL_DATE);
+
+        LocalDate regionValueDate = null;
+        if (dto.getRegionValueDate() != null) {
+            regionValueDate = LocalDate.parse(dto.getRegionValueDate(), DateTimeFormatter.ISO_LOCAL_DATE);
+        }
+
+        Long billAmountCents = (new BigDecimal(dto.getBillAmount().replace(',', '.'))).multiply(BigDecimal.valueOf(100)).longValue();
+
+        return Treasury.builder()
+                .billAmountCents(billAmountCents)
+                .billYear(dto.getBillYear())
+                .billCode(dto.getBillCode())
+                .billDate(billDate)
+                .ingestionFlowFileId(ingestionFlowFile.getIngestionFlowFileId())
+                .iuf(TreasuryUtils.getIdentificativo(dto.getRemittanceDescription(), TreasuryUtils.IUF))
+                .organizationId(ingestionFlowFile.getOrganizationId())
+                .orgBtCode(ORG_BT_CODE_DEFAULT)
+                .orgIstatCode(ORG_ISTAT_CODE_DEFAULT)
+                .pspLastName(dto.getPspLastName())
+                .regionValueDate(regionValueDate)
+                .remittanceDescription(dto.getRemittanceDescription())
+                .treasuryOrigin(TreasuryOrigin.TREASURY_CSV)
+                .build();
+    }
+}
