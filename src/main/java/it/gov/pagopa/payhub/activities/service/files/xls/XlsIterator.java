0ac0717f8a8588383a5291927f290f6959d04aa7
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2025-09-25 16:22:59 +0200

    P4ADEV-3795-fix-activity-TreasuryXlsIngestionActivity fix minor bug in XlsIterator.buildRow (replace all null value with empty string, but this two values are managed equally by TreasuryXlsRowMapper)

--- src/main/java/it/gov/pagopa/payhub/activities/service/files/xls/XlsIterator.java
+++ src/main/java/it/gov/pagopa/payhub/activities/service/files/xls/XlsIterator.java
@@ -151,9 +151,8 @@ public class XlsIterator<D> implements Closeable, Iterator<D> {
 	}
 
 	private List<String> buildRow(Map<Integer, String> rowBuffer) {
-	 	List<String> cells = Arrays.asList(new String[computeHeaderCount()]);
+	 	List<String> cells = new ArrayList<>(Collections.nCopies(computeHeaderCount(), ""));
 		rowBuffer.forEach(cells::set);
-		IntStream.range(0, computeHeaderCount()-rowBuffer.size()).forEach(i -> cells.set(rowBuffer.size() + i, ""));
 		return cells;
 	}
 
