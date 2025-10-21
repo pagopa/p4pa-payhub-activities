f22a6d828f52fb366d225f2fc75c5e48ce918e8a
antoniocalo <antonio.calo@nttdata.com>
2025-10-21 18:35:10 +0200

    fix-pair-to-classifyTransfer

--- src/main/java/it/gov/pagopa/payhub/activities/activity/classifications/TransferClassificationActivity.java
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/classifications/TransferClassificationActivity.java
@@ -5,7 +5,7 @@ import io.temporal.activity.ActivityMethod;
 import it.gov.pagopa.payhub.activities.dto.classifications.TransferSemanticKeyDTO;
 import it.gov.pagopa.pu.debtposition.dto.generated.InstallmentNoPII;
 import it.gov.pagopa.pu.debtposition.dto.generated.Transfer;
-import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
 /**
  * Interface for defining an activity to process Transfer classifications.
@@ -20,5 +20,5 @@ public interface TransferClassificationActivity {
 	 * @return Pair&lt;InstallmentNoPII,Transfer&gt; return pair of classified installment and transfer
 	 */
 	@ActivityMethod
-	Pair<InstallmentNoPII,Transfer> classifyTransfer(TransferSemanticKeyDTO transferSemanticKey);
+	ImmutablePair<InstallmentNoPII,Transfer> classifyTransfer(TransferSemanticKeyDTO transferSemanticKey);
 }
