32d01dd517b4dbafdfc4c3975aa39455da41211d
ElisKina-dev <144669238+ElisKina-dev@users.noreply.github.com>
2025-10-22 12:45:44 +0200

    fix: P4ADEV-4076 change mappingStrategy in createCsv (#451)

--- src/main/java/it/gov/pagopa/payhub/activities/service/files/CsvService.java
+++ src/main/java/it/gov/pagopa/payhub/activities/service/files/CsvService.java
@@ -26,6 +26,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Supplier;
+
+import it.gov.pagopa.payhub.activities.mapper.OrderedHeaderColumnNameMappingStrategy;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Value;
@@ -113,8 +115,7 @@ public class CsvService {
         }
 
         try (Writer writer = Files.newBufferedWriter(csvFilePath)) {
-            HeaderColumnNameMappingStrategy<C> mappingStrategy = new HeaderColumnNameMappingStrategy<>();
-            mappingStrategy.setProfile(csvProfile);
+            OrderedHeaderColumnNameMappingStrategy<C> mappingStrategy = new OrderedHeaderColumnNameMappingStrategy<>();
             mappingStrategy.setType(typeClass);
 
             StatefulBeanToCsv<C> beanToCsv = new StatefulBeanToCsvBuilder<C>(writer)
