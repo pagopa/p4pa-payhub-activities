43854bbd9cc3ad7122ddf04ce66192008c944aed
antoniocalo <antonio.calo@nttdata.com>
2025-10-27 10:12:14 +0100

    fix-transferclassify

--- src/main/java/it/gov/pagopa/payhub/activities/activity/classifications/TransferClassificationActivity.java
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/classifications/TransferClassificationActivity.java
@@ -2,10 +2,8 @@ package it.gov.pagopa.payhub.activities.activity.classifications;
 
 import io.temporal.activity.ActivityInterface;
 import io.temporal.activity.ActivityMethod;
+import it.gov.pagopa.payhub.activities.dto.classifications.TransferClassifyDTO;
 import it.gov.pagopa.payhub.activities.dto.classifications.TransferSemanticKeyDTO;
-import it.gov.pagopa.pu.debtposition.dto.generated.InstallmentNoPII;
-import it.gov.pagopa.pu.debtposition.dto.generated.Transfer;
-import org.apache.commons.lang3.tuple.Pair;
 
 /**
  * Interface for defining an activity to process Transfer classifications.
@@ -20,5 +18,5 @@ public interface TransferClassificationActivity {
 	 * @return Pair&lt;InstallmentNoPII,Transfer&gt; return pair of classified installment and transfer
 	 */
 	@ActivityMethod
-	Pair<InstallmentNoPII,Transfer> classifyTransfer(TransferSemanticKeyDTO transferSemanticKey);
+	TransferClassifyDTO classifyTransfer(TransferSemanticKeyDTO transferSemanticKey);
 }
