5d0d90eda6cdb887a9d1bbd09e7f4d25db41686d
Benedetta-fabbri <benedetta11.fabbri@emea.nttdata.com>
2026-02-18 16:36:36 +0100

    [P4ADEV-4486] fix file output DP import

--- src/main/java/it/gov/pagopa/payhub/activities/mapper/exportflow/debtposition/IUVInstallmentsExportFlowFileDTOMapper.java
+++ src/main/java/it/gov/pagopa/payhub/activities/mapper/exportflow/debtposition/IUVInstallmentsExportFlowFileDTOMapper.java
@@ -4,9 +4,11 @@ import it.gov.pagopa.payhub.activities.dto.exportflow.debtposition.IUVInstallmen
 import it.gov.pagopa.pu.debtposition.dto.generated.DebtPositionTypeOrg;
 import it.gov.pagopa.pu.debtposition.dto.generated.InstallmentDTO;
 import it.gov.pagopa.pu.debtposition.dto.generated.PersonDTO;
+import it.gov.pagopa.pu.debtposition.dto.generated.TransferDTO;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 import static it.gov.pagopa.payhub.activities.util.Utilities.longCentsToBigDecimalEuro;
@@ -17,31 +19,48 @@ public class IUVInstallmentsExportFlowFileDTOMapper {
 
     public IUVInstallmentsExportFlowFileDTO map(InstallmentDTO dto, DebtPositionTypeOrg debtPositionTypeOrg) {
         PersonDTO debtor = dto.getDebtor();
+        List<TransferDTO> transferDTOList = dto.getTransfers();
 
-        return IUVInstallmentsExportFlowFileDTO.builder()
-            .iud(dto.getIud())
-            .iuv(dto.getIuv())
-            .entityType(debtor.getEntityType())
-            .fiscalCode(debtor.getFiscalCode())
-            .fullName(debtor.getFullName())
-            .address(debtor.getAddress())
-            .civic(debtor.getCivic())
-            .postalCode(debtor.getPostalCode())
-            .location(debtor.getLocation())
-            .province(debtor.getProvince())
-            .nation(debtor.getNation())
-            .email(debtor.getEmail())
-            .dueDate(dto.getDueDate())
-            .amount(longCentsToBigDecimalEuro(dto.getAmountCents()))
-            .paCommissionCents(dto.getNotificationFeeCents())
-            .debtPositionTypeCode(debtPositionTypeOrg.getCode())
-            .remittanceInformation(Optional.ofNullable(
-                dto.getOriginalRemittanceInformation())
-                .orElse(dto.getRemittanceInformation()))
-            .legacyPaymentMetadata(dto.getLegacyPaymentMetadata())
-            .balance(dto.getBalance())
-            .flagPuPagoPaPayment(Boolean.TRUE)
-            .action(dto.getIngestionFlowFileAction())
-            .build();
+         IUVInstallmentsExportFlowFileDTO iuvInstallmentsExportFlowFileDTO = IUVInstallmentsExportFlowFileDTO.builder()
+                .iud(dto.getIud())
+                .iuv(dto.getIuv())
+                .entityType(debtor.getEntityType())
+                .fiscalCode(debtor.getFiscalCode())
+                .fullName(debtor.getFullName())
+                .address(debtor.getAddress())
+                .civic(debtor.getCivic())
+                .postalCode(debtor.getPostalCode())
+                .location(debtor.getLocation())
+                .province(debtor.getProvince())
+                .nation(debtor.getNation())
+                .email(debtor.getEmail())
+                .dueDate(dto.getDueDate())
+                .amount(longCentsToBigDecimalEuro(dto.getAmountCents()))
+                .debtPositionTypeCode(debtPositionTypeOrg.getCode())
+                .paymentType("ALL")
+                .remittanceInformation(Optional.ofNullable(
+                                dto.getOriginalRemittanceInformation())
+                        .orElse(dto.getRemittanceInformation()))
+                .legacyPaymentMetadata(dto.getLegacyPaymentMetadata())
+                .balance(dto.getBalance())
+                .generateNotice(dto.getGenerateNotice())
+                .flagMultiBeneficiary(transferDTOList.size() == 1 ? Boolean.FALSE : Boolean.TRUE)
+                .action(dto.getIngestionFlowFileAction())
+                .build();
+
+         TransferDTO transfer2 = transferDTOList.stream()
+                 .filter(transferDTO -> transferDTO.getTransferIndex() == 2)
+                 .findFirst().orElse(null);
+
+         if(transfer2 != null) {
+            iuvInstallmentsExportFlowFileDTO.setOrgFiscalCode2(transfer2.getOrgFiscalCode());
+            iuvInstallmentsExportFlowFileDTO.setOrgName2(transfer2.getOrgName());
+            iuvInstallmentsExportFlowFileDTO.setOrgIban2(transfer2.getIban());
+            iuvInstallmentsExportFlowFileDTO.setOrgLegacyPaymentMetadata2(transfer2.getCategory());
+            iuvInstallmentsExportFlowFileDTO.setOrgRemittanceInformation2(transfer2.getRemittanceInformation());
+            iuvInstallmentsExportFlowFileDTO.setOrgAmount2(longCentsToBigDecimalEuro(transfer2.getAmountCents()));
+         }
+
+         return iuvInstallmentsExportFlowFileDTO;
     }
 }
