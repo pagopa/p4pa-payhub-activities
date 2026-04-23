f74cd74c3f2c6c16a60cdbb06e86c64c9e6afc3c
Leonardo Pierangelini <leonardo.pierangelini@nttdata.com>
2026-04-23 19:55:23 +0200

    P4ADEV-4599-broker-personalization-for-receipt-email: fetch brokerId, needed for fetching email template from repo, for all email types (not only for receipt); fetch mail_sender_address for all email types (not only for receipt).

--- src/main/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/email/SendEmailIngestionFlowActivityImpl.java
+++ src/main/java/it/gov/pagopa/payhub/activities/activity/ingestionflow/email/SendEmailIngestionFlowActivityImpl.java
@@ -1,13 +1,18 @@
 package it.gov.pagopa.payhub.activities.activity.ingestionflow.email;
 
 import it.gov.pagopa.payhub.activities.activity.email.SendEmailActivity;
+import it.gov.pagopa.payhub.activities.connector.organization.BrokerService;
+import it.gov.pagopa.payhub.activities.connector.organization.OrganizationService;
 import it.gov.pagopa.payhub.activities.connector.processexecutions.IngestionFlowFileService;
 import it.gov.pagopa.payhub.activities.dto.email.TemplatedEmailDTO;
 import it.gov.pagopa.payhub.activities.enums.EmailTemplateName;
 import it.gov.pagopa.payhub.activities.exception.ingestionflow.IngestionFlowFileNotFoundException;
+import it.gov.pagopa.payhub.activities.exception.organization.OrganizationNotFoundException;
 import it.gov.pagopa.payhub.activities.service.ingestionflow.email.IngestionFlowFileEmailContentConfigurerService;
 import it.gov.pagopa.payhub.activities.service.ingestionflow.email.IngestionFlowFileEmailDestinationRetrieverService;
 import it.gov.pagopa.payhub.activities.service.ingestionflow.email.IngestionFlowFileEmailTemplateResolverService;
+import it.gov.pagopa.pu.organization.dto.generated.BrokerConfiguration;
+import it.gov.pagopa.pu.organization.dto.generated.Organization;
 import it.gov.pagopa.pu.processexecutions.dto.generated.IngestionFlowFile;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
@@ -15,6 +20,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Lazy
 @Slf4j
@@ -26,13 +32,17 @@ public class SendEmailIngestionFlowActivityImpl implements SendEmailIngestionFlo
     private final IngestionFlowFileEmailDestinationRetrieverService destinationRetrieverService;
     private final IngestionFlowFileEmailContentConfigurerService contentConfigurerService;
     private final SendEmailActivity sendEmailActivity;
+    private final OrganizationService organizationService;
+    private final BrokerService brokerService;
 
-    public SendEmailIngestionFlowActivityImpl(IngestionFlowFileService ingestionFlowFileService, IngestionFlowFileEmailTemplateResolverService emailTemplateResolverService, IngestionFlowFileEmailDestinationRetrieverService destinationRetrieverService, IngestionFlowFileEmailContentConfigurerService contentConfigurerService, SendEmailActivity sendEmailActivity) {
+    public SendEmailIngestionFlowActivityImpl(IngestionFlowFileService ingestionFlowFileService, IngestionFlowFileEmailTemplateResolverService emailTemplateResolverService, IngestionFlowFileEmailDestinationRetrieverService destinationRetrieverService, IngestionFlowFileEmailContentConfigurerService contentConfigurerService, SendEmailActivity sendEmailActivity, OrganizationService organizationService, BrokerService brokerService) {
         this.ingestionFlowFileService = ingestionFlowFileService;
         this.emailTemplateResolverService = emailTemplateResolverService;
         this.destinationRetrieverService = destinationRetrieverService;
         this.contentConfigurerService = contentConfigurerService;
         this.sendEmailActivity = sendEmailActivity;
+        this.organizationService = organizationService;
+        this.brokerService = brokerService;
     }
 
 
@@ -40,17 +50,26 @@ public class SendEmailIngestionFlowActivityImpl implements SendEmailIngestionFlo
     public void sendIngestionFlowFileCompleteEmail(Long ingestionFlowFileId, boolean success) {
         log.info("Sending email for IngestionFlowFile {} with success {}", ingestionFlowFileId, success);
         IngestionFlowFile ingestionFlowFileDTO = retrieveIngestionFlowFileRecord(ingestionFlowFileId);
+        Organization organization = retrieveOrganization(ingestionFlowFileDTO.getOrganizationId());
+        String mailSenderAddress = Optional.ofNullable(brokerService.getBrokerConfigurationsById(organization.getBrokerId()))
+                .map(BrokerConfiguration::getMailSenderAddress)
+                .orElse(null);
 
         EmailTemplateName templateName = emailTemplateResolverService.resolve(ingestionFlowFileDTO, success);
         Map<String, String> params = contentConfigurerService.configureParams(ingestionFlowFileDTO, success);
         Pair<String[], String[]> destinations = destinationRetrieverService.retrieveEmailDestinations(ingestionFlowFileDTO);
 
-        TemplatedEmailDTO templatedEmail = new TemplatedEmailDTO(templateName, null, destinations.getKey(), destinations.getValue(), params, null);
-        sendEmailActivity.sendTemplatedEmail(null, templatedEmail);
+        TemplatedEmailDTO templatedEmail = new TemplatedEmailDTO(templateName, mailSenderAddress, destinations.getKey(), destinations.getValue(), params, null);
+        sendEmailActivity.sendTemplatedEmail(organization.getBrokerId(), templatedEmail);
     }
 
     private IngestionFlowFile retrieveIngestionFlowFileRecord(Long ingestionFlowFileId) {
         return ingestionFlowFileService.findById(ingestionFlowFileId)
                 .orElseThrow(() -> new IngestionFlowFileNotFoundException("Cannot find ingestionFlow having id: " + ingestionFlowFileId));
+    }
+
+    private Organization retrieveOrganization(Long organizationId) {
+        return organizationService.getOrganizationById(organizationId)
+                .orElseThrow(() -> new OrganizationNotFoundException("Cannot find Organization having id: " + organizationId));
     }
 }
