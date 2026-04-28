4ebea85a8ff23e163bc8310aad9dfa9031b6967a
antonio.torre <antonio.torre@pagopa.it>
2026-04-28 19:25:31 +0200

    fix: Aligned openApi

--- src/test/java/it/gov/pagopa/payhub/activities/util/faker/InstallmentSynchronizeDTOFaker.java
+++ src/test/java/it/gov/pagopa/payhub/activities/util/faker/InstallmentSynchronizeDTOFaker.java
@@ -1,10 +1,10 @@
 package it.gov.pagopa.payhub.activities.util.faker;
 
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import it.gov.pagopa.pu.debtposition.dto.generated.Action;
 import it.gov.pagopa.pu.debtposition.dto.generated.InstallmentSynchronizeDTO;
 import it.gov.pagopa.pu.debtposition.dto.generated.PersonEntityType;
 import it.gov.pagopa.pu.debtposition.dto.generated.TransferSynchronizeDTO;
+import tools.jackson.databind.node.JsonNodeFactory;
 
 import java.util.List;
 
