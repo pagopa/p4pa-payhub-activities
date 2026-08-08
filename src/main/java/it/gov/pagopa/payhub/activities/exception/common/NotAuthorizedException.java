972d564c196fd9093c0e8bb7aabfd1a7ed803bbb
antonio.torre <antonio.torre@pagopa.it>
2026-08-08 15:48:14 +0200

    fix: Returning field error details

--- src/main/java/it/gov/pagopa/payhub/activities/exception/common/NotAuthorizedException.java
+++ src/main/java/it/gov/pagopa/payhub/activities/exception/common/NotAuthorizedException.java
@@ -1,9 +1,9 @@
 package it.gov.pagopa.payhub.activities.exception.common;
 
-import it.gov.pagopa.payhub.activities.exception.NotRetryableActivityException;
+import it.gov.pagopa.payhub.activities.exception.RetryableActivityException;
 
 @SuppressWarnings("java:S110") // Suppress "Inheritance tree of classes should not be too deep": allowed for exception hierarchy
-public class NotAuthorizedException extends NotRetryableActivityException {
+public class NotAuthorizedException extends RetryableActivityException {
 
     public NotAuthorizedException(String code, String message) {
         super(code, message);
